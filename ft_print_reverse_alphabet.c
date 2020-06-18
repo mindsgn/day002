e917096c16c55b7f9bc1b2206efebf57d0f8da0f
Sibongiseni Tembe <stembe@c3r2s1.wethinkcode.co.za>
2020-06-18 09:58:43 +0200

    first commit

---
+++ ft_print_reverse_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <string.h>
+
+/*
+* print the alphabet in ascending order, in lowercase, on the same line.
+*/
+
+int main(){
+	//illegal
+	fputs('sssss', stdout);
+	return 0;
+}
+
+void (void){
+	
+}
