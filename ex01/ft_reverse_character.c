473d4d10fba224aaef4f202308cc5a6c57ff3f70
Sibongiseni Tembe <stembe@c3r2s1.wethinkcode.co.za>
2020-06-19 17:28:02 +0200

    ex01

--- ex01/ft_reverse_character.c
+++ ex01/ft_reverse_character.c
@@ -6,20 +6,18 @@
 /*   By: stembe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 17:22:36 by stembe            #+#    #+#             */
-/*   Updated: 2020/06/19 17:18:52 by stembe           ###   ########.fr       */
+/*   Updated: 2020/06/19 17:26:41 by stembe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
-#include <unistd.h>
 #include "ft_putchar.c"
 
-int ft_print_alphabet(void){
+void ft_print_reverse_alphabet(void){
 	char letter;
-	letter='a';
-	while(letter<='z'){
+	letter='z';
+	while(letter<='a'){
 		ft_putchar(letter);		
-		++letter;
+		--letter;
 	}
-	return 0;
 }
