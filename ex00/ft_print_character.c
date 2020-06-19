33a14c84f836d5a699186f1db2f44afb99f68e37
Sibongiseni Tembe <stembe@c3r2s1.wethinkcode.co.za>
2020-06-19 17:07:03 +0200

    test

--- ex00/ft_print_character.c
+++ ex00/ft_print_character.c
@@ -6,16 +6,13 @@
 /*   By: stembe <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/06/18 17:22:36 by stembe            #+#    #+#             */
-/*   Updated: 2020/06/19 15:17:58 by stembe           ###   ########.fr       */
+/*   Updated: 2020/06/19 17:01:11 by stembe           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <stdio.h>
 #include <unistd.h>
-
-void ft_putchar(char letter){
-	write(STDOUT_FILENO, &letter, 1);
-}
+#include "ft_putchar.c"
 
 void ft_print_alphabet(void){
 	char letter;
@@ -24,9 +21,4 @@ void ft_print_alphabet(void){
 		ft_putchar(letter);		
 		++letter;
 	}
-}
-
-int main(){
-	ft_print_alphabet();
-	return 0;
 }
